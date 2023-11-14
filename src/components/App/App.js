3de295256071e17789ae6fafceb53cca36d8518a
Andrey Lebedev <alebedev424@gmail.com>
2023-11-14 15:57:02 +0300

    feat: creat LoginForm component

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import LoginForm from '../LoginForm/LoginForm'
+
+function App() {
+  return (
+    <div className="page">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/Api.js
@@ -0,0 +1,24 @@
+const BASE_URL = 'https://auth.nomoreparties.co';
+
+const makeRequest = (url, method, body) => {
+  const options = {
+    method,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }
+  if (body) {
+    options.body = JSON.stringify(body)
+  }
+  return fetch(`${BASE_URL}/${url}`, options)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error('Error occurred!')
+      }
+      return res.json()
+    })
+}
+
+export const authorize = (email, password) => {
+  return makeRequest('signin', 'POST', { email, password })
+}
